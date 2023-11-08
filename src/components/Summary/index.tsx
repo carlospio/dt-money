f79e08b666e584dc8fd13e60edcbdd33b2a11aa9
Carlos Pio <carlospiobenicio@gmail.com>
2023-11-08 11:54:17 -0300

    Estrutura visual - Componente: Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,40 @@
+import { SummaryCard, SummaryContainer } from "./styles";
+import {ArrowCircleDown, ArrowCircleUp, CurrencyDollar} from 'phosphor-react'
+
+export function Summary() {
+    return (
+        <SummaryContainer>
+            <SummaryCard>
+                <header>
+                    <span>Entradas</span>
+
+                    <ArrowCircleUp size={32} color="#00b37e" />
+
+                    <strong>R$ 17.488,00</strong>
+                </header>
+            </SummaryCard>
+
+            <SummaryCard>
+                <header>
+                    <span>Saídas</span>
+
+                    <ArrowCircleDown size={32} color="#f75a68" />
+
+                    <strong>R$ 17.488,00</strong>
+                </header>
+            </SummaryCard>
+
+            <SummaryCard variant="green">
+                <header>
+                    <span>Total</span>
+
+                    <CurrencyDollar size={32} color="#fff" />
+
+                    <strong>R$ 17.488,00</strong>
+                </header>
+            </SummaryCard>
+        </SummaryContainer>
+    )
+
+
+}
