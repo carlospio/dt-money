1139113aedf6ab89bed6777d99b82aa1feee1097
Carlos Pio <carlospiobenicio@gmail.com>
2023-12-14 11:40:13 -0300

    Estrutura visual - Componente: SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,38 +1,44 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContainer, TransactionsTable, PriceHighLight  } from "./styles";
+import { SearchForm } from "./components/SearchForm";
 
 
 export function Transactions(){
     return( 
     <div> 
         <Header />
+
         <Summary />
 
+      
+
         <TransactionsContainer>
-        <TransactionsTable>
-            <tbody>
-                <tr>
-                    <td width="40%">Desenvolvimento de site</td>
-                    <td>
-                        <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
-                    </td>
-                    <td>Venda</td>
-                    <td>13/04/2022</td>
-                </tr>
 
-                <tr>
-                    <td width="40%">Ifood</td>
-                    <td>
-                        <PriceHighLight variant="outcome">- R$ 59,90</PriceHighLight>
-                    </td>
-                    <td>Alimentação</td>
-                    <td>10/04/2022</td>
-                </tr>
+            <SearchForm />
+            <TransactionsTable>
+                <tbody>
+                    <tr>
+                        <td width="40%">Desenvolvimento de site</td>
+                        <td>
+                            <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+                        </td>
+                        <td>Venda</td>
+                        <td>13/04/2022</td>
+                    </tr>
 
-             
-            </tbody>
-        </TransactionsTable>
+                    <tr>
+                        <td width="40%">Ifood</td>
+                        <td>
+                            <PriceHighLight variant="outcome">- R$ 59,90</PriceHighLight>
+                        </td>
+                        <td>Alimentação</td>
+                        <td>10/04/2022</td>
+                    </tr>
+
+                
+                </tbody>
+            </TransactionsTable>
         </TransactionsContainer>
 
 
