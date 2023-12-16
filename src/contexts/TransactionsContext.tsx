b4d39c474a8bb0f7c9efd1e1eee8d0e5a527ef1c
Carlos Pio <carlospiobenicio@gmail.com>
2023-12-16 11:42:48 -0300

    Conexão com a API - Configurando Axios

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useEffect, useState} from 'react'
+import { createContext, useEffect, useState, ReactNode} from 'react'
+import { api } from '../lib/axios';
 
 
 
@@ -32,16 +33,12 @@ export function TransactionProvider({children}: TransactionsProviderProps){
     const [transactions, setTransactions] = useState<Transaction[]>([]);
 
     async function fetchTransactions(query?: string) {
-        const url = new URL('http://localhost:3000/transactions')
-
-        if(query){
-            url.searchParams.append('q', query)
-        }
-
-        const response = await fetch(url);
-        const data = await response.json();
-
-        setTransactions(data)
+        const response = await api.get('/transactions', {
+            params: {
+                q: query,
+            }
+        })
+        setTransactions(response.data)
     }
 
     useEffect(() => {
